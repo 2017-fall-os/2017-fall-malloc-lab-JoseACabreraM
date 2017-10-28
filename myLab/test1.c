5940ef9b30758f8f6bb2d9d7e887604c1cd7d12a
JoseACabreraM <jacabreramaynez@miners.utep.edu>
2017-10-28 17:55:36 -0600

    Add files via upload

--- myLab/test1.c
+++ myLab/test1.c
@@ -5,76 +5,49 @@
 #include <sys/resource.h>
 #include <unistd.h>
 
-double diffTimeval(struct timeval *t1, struct timeval *t2) {
-    double d = (t1->tv_sec - t2->tv_sec) + (1.0e-6 * (t1->tv_usec - t2->tv_usec));
-    return d;
+double diffTimeval1(struct timeval *t1, struct timeval *t2) {
+  double d = (t1->tv_sec - t2->tv_sec) + (1.0e-6 * (t1->tv_usec - t2->tv_usec));
+  return d;
 }
 
-void getutime(struct timeval *t) {
-    struct rusage usage;
-    getrusage(RUSAGE_SELF, &usage);
-    *t = usage.ru_utime;
+void getutime1(struct timeval *t)
+{
+  struct rusage usage;
+  getrusage(RUSAGE_SELF, &usage);
+  *t = usage.ru_utime;
 }
 
-int main() {
-    void *p1, *p2, *p3;
-    int mallocTest;
 
-    printf("Which allocator to test. 1 for malloc, 0 for myAllocator: ");
-    scanf("%d", &mallocTest);
 
-    if (mallocTest) {
-        printf("Testing malloc\n");
-        arenaCheck();
-        p1 = malloc(254);
-        arenaCheck();
-        p2 = malloc(25400);
-        arenaCheck();
-        p3 = malloc(254);
-        printf("%p %p %p\n", p1, p2, p3);
-        arenaCheck();
-        free(p2);
-        arenaCheck();
-        free(p3);
-        arenaCheck();
-        free(p1);
-        arenaCheck();
-        {                /* measure time for 10000 mallocs */
-            struct timeval t1, t2;
-            int i;
-            getutime(&t1);
-            for (i = 0; i < 10000; i++)
-                if (malloc(4) == 0)
-                    break;
-            getutime(&t2);
-            printf("%d malloc(4) required %f seconds\n", i, diffTimeval(&t2, &t1));
-        }
-    } else {
-        printf("Testing myAllocator\n");
-        arenaCheck();
-        p1 = bestFitAllocRegion(254);
-        arenaCheck();
-        p2 = bestFitAllocRegion(25400);
-        arenaCheck();
-        p3 = bestFitAllocRegion(254);
-        printf("%p %p %p\n", p1, p2, p3);
-        arenaCheck();
-        freeRegion(p2);
-        arenaCheck();
-        freeRegion(p3);
-        arenaCheck();
-        freeRegion(p1);
-        arenaCheck();
-        {                /* measure time for 10000 mallocs */
-            struct timeval t1, t2;
-            int i;
-            getutime(&t1);
-            for (i = 0; i < 10000; i++)
-                if (firstFitAllocRegion(4) == 0)
-                    break;
-            getutime(&t2);
-            printf("%d firstFitAllocRegion(4) required %f seconds\n", i, diffTimeval(&t2, &t1));
-        }
-    }
-    return 0;
+int main2()
+{
+  void *p1, *p2, *p3;
+  arenaCheck();
+  p1 = malloc(254);
+  arenaCheck();
+  p2 = malloc(25400);
+  arenaCheck();
+  p3 = malloc(254);
+  printf("%8zx %8zx %8zx\n", p1, p2, p3);
+  arenaCheck();
+  free(p2);
+  arenaCheck();
+  free(p3);
+  arenaCheck();
+  free(p1);
+  arenaCheck();
+  {				/* measure time for 10000 mallocs */
+    struct timeval t1, t2;
+    int i;
+    getutime1(&t1);
+    for(i = 0; i < 100000; i++)
+      if (malloc(4) == 0) 
+	break;
+    getutime1(&t2);
+    printf("%d malloc(4) required %f seconds\n", i, diffTimeval1(&t2, &t1));
+  }
+  return 0;
 }
+
+
+
